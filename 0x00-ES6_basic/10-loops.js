de6f9847519138d2c536fbef370e67f77a8c1784
mudauzwothe <mudauzwothe@gmail.com>
2024-05-27 22:02:00 +0000

    hezy

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let result = [];
+  for (let value of array) {
+    result.push(appendString + value);
+  }
+
+  return result;
+}
+
